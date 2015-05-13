62a1f7d1d6286ab5638e23d32249adc5f7f91a8b
PRADEEP BHADURIA <prbhadur@cisco.com>
2015-05-13 22:28:43 +0530

    template for seats availablity

--- routes.js
+++ routes.js
@@ -5,6 +5,9 @@ module.exports = function (app) {
     app.get('/', main.home);
     app.post('/trains', main.getTrains);
     app.post('/pnr', main.getTrains);
+    app.post('/route', main.getTrains);
+    app.post('/live', main.getTrains);
+    app.post('/seats', main.getTrains);
    /* app.get('/about', main.about);
     app.get('/thank-you', main.thankYou);
     app.get('/newsletter', main.newsletter);
